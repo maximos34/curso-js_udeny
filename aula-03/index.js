13cb0bb7e7c54c1309987d77a93c03deb3d2c2d1
pedro falcao <maximosjoselages@gmail.com>
2022-06-28 09:51:38 -0300

    aula12

---
+++ aula-03/index.js
@@ -0,0 +1,3 @@
+
+
+console.log('Meu nome é "pedro".Estou aprendendo Javascript às ', 08 ,' da manhã');
---
+++ aula-05/js/index.js
@@ -0,0 +1,4 @@
+
+console.log('oi')
+console.log('vou ser o melhor programador do mundo')
+alert('te amo')
